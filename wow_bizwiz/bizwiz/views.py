8651c65b2d2f14e3a2aff47be43c2662cdfe841d
Darien O. Davis <dariendarien03@gmail.com>
2022-05-09 00:06:00 -0400

    Homepage Says Hello

--- wow_bizwiz/bizwiz/views.py
+++ wow_bizwiz/bizwiz/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+from bizwiz.models import *
 
 # Create your views here.
 
-class Home():
-    pass
+class Home(View):
+    def get(self, request):
+
+        return render(
+            request=request, template_name = 'home.html', context = {}
+        )
+

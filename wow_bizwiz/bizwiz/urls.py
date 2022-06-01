8bb8a1d8ee32f215b470316c9cfc131c23b16184
Sanford Barber <Sanfordbarber30@yahoo.com>
2022-06-01 09:41:47 -0400

    added signin, signup, signout

--- wow_bizwiz/bizwiz/urls.py
+++ wow_bizwiz/bizwiz/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from bizwiz.views import * 
+from django.contrib import admin
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
+ path('', views.homee, name="homee"),
+ path('signup', views.signup, name="signup"),
+ path('signin', views.signin, name="signin"),
+ path('signout', views.signout, name="signout"),
  path('', HomeView.as_view(), name='home'),
  path('industry/<int:industry_id>/', IndustryView.as_view(), name='industry'),
  path('question/<int:industry_id>/<int:question_id>/', Specific_QuestionView.as_view(), name= 'specific_question'),

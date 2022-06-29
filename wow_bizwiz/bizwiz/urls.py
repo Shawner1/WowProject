4420662f3a953a4ae8f132ef1f38d45f8027ad3d
Darien O. Davis <dariendarien03@gmail.com>
2022-06-29 19:14:07 -0400

    url addition

--- wow_bizwiz/bizwiz/urls.py
+++ wow_bizwiz/bizwiz/urls.py
@@ -3,6 +3,10 @@ from bizwiz.views import *
 
 urlpatterns = [
  path('', HomeView.as_view(), name='home'),
+path('', views.homee, name="landing"),
+ path('signup', views.signup, name="signup"),
+ path('signin', views.signin, name="signin"),
+ path('signout', views.signout, name="signout"),
  path('industry/<int:industry_id>/', IndustryView.as_view(), name='industry'),
  path('question/<int:industry_id>/<int:question_id>/', Specific_QuestionView.as_view(), name= 'specific_question'),
  path('tags/<int:tag_id>/', Page_for_TagsView.as_view(), name='page_for_tags'),
